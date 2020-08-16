9cc7c903c4e0dccd04a081804817a4fadb70ff1d
Rosario Holsen <rholsen@gmail.com>
2020-08-16 21:11:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,13 @@ def countdown(number)
   end
     return "HAPPY NEW YEAR!"
 end
+
+number == 11
+def countdown_with_sleep(number)
+  while number > 0 
+    puts  "#{number} SECOND(S)!"
+    number -= 1
+    sleep 5
+  end
+  return "HAPPY NEW YEAR!"
+end 
